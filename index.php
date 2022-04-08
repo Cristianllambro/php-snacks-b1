e95d453a1f9c43e6c06ffae699ce3fc4a3d6708b
cristianllambro <llambro.cristian1996@gmail.comm>
2022-04-08 15:51:35 +0200

    sdfjkop[

--- index.php
+++ index.php
@@ -7,6 +7,48 @@
     <title>Snacks PHP</title>
 </head>
 <body>
-    
+
+    <!-- SNAKE NUMBER ONE -->
+    <div class="snak1">
+        <h1>Snack number 1</h1>
+        <?php $arr_game= 
+            [
+                'home' => [
+                    'name' => 'Olimpia Milano',
+                    'score' => 55,
+                ],
+                'out' => [
+                    'name' => 'Cantu',
+                    'score' => 60,
+                ],
+                'home' => [
+                    'name' => 'Reyer',
+                    'score' => 120,
+                ],
+                'out' => [
+                    'name' => 'Treviso',
+                    'score' => 1,
+                ],
+            ];
+
+            for ($_i = 0; $_i < count($arr_game); $_i++) { 
+                echo $arr_game[$_i]['home']['name'];
+            }
+        ?>
+
+    </div>
+
+
+    <!-- SNAKE NUMBER TWO -->
+    <div class="two">
+        <h1>Snack number 2</h1>
+        <?php ?>
+
+        <form action="">
+            <label for="email">Enter your email</label>
+            <input type="email">
+            <button>Done</button>
+        </form>
+    </div>
 </body>
 </html>
